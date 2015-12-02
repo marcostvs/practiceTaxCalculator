100a092088a942ffdf324741896735c8876df764
marcostvs <erasmomarcoesteves@gmail.com>
2015-12-02 15:52:20 -0800

    Initial Commit

---
+++ practiceTaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practiceTaxCalculator
+//
+//  Created by Erasmo Marco Esteves on 12/2/15.
+//  Copyright © 2015 Erasmo Esteves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practiceTaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practiceTaxCalculator
+//
+//  Created by Erasmo Marco Esteves on 12/2/15.
+//  Copyright © 2015 Erasmo Esteves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
